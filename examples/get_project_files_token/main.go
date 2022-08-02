43b5e74d1710338fc2aefeb919ed1f4d4b5da10e
zhangdapeng520 <pygosuperman@outlook.com>
2022-08-02 16:00:36 +0800

    新增：获取指定项目中每个文件的token

---
+++ examples/get_project_files_token/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/zhangdapeng520/zdpgo_clearcode"
+	"github.com/zhangdapeng520/zdpgo_file"
+	"github.com/zhangdapeng520/zdpgo_pygments"
+	"github.com/zhangdapeng520/zdpgo_pygments/lexers"
+	"strings"
+)
+
+func main() {
+	// 获取指定项目中每个文件的token
+	handleFunc := func(filePath string) {
+		if strings.HasSuffix(filePath, ".py") {
+			// 代码清洗
+			content, err := zdpgo_clearcode.ClearCode(filePath)
+			if err != nil {
+				fmt.Println("代码清洗失败：", err)
+				return
+			}
+
+			// 词法分析获取token
+			lexer := lexers.Match(filePath)
+			token, err := zdpgo_pygments.GetToken(lexer, content)
+			if err != nil {
+				fmt.Println("词法分析获取token失败：", err)
+				return
+			}
+
+			fmt.Println(filePath)
+			fmt.Println(token)
+			fmt.Println()
+		}
+	}
+
+	// 被忽略的文件夹
+	ignoreDirs := []string{"venv", ".git", ".idea"}
+
+	// 执行处理方法
+	err := zdpgo_file.HandleDirFileWithIgnoreDirList("D:\\zdppy\\django_for_api_4.0", handleFunc, ignoreDirs)
+	if err != nil {
+		fmt.Println("处理项目源码出错：", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -4,10 +4,11 @@ go 1.18
 
 require (
 	github.com/zhangdapeng520/zdpgo_clearcode v0.1.0
+	github.com/zhangdapeng520/zdpgo_file v1.0.9
 	github.com/zhangdapeng520/zdpgo_pygments v0.1.1
 	golang.org/x/text v0.3.7
 )
 
 //replace (
-//	github.com/zhangdapeng520/zdpgo_pygments v0.1.0 => ../zdpgo_pygments
+//	github.com/zhangdapeng520/zdpgo_file v1.0.9 => ../zdpgo_file
 //)
